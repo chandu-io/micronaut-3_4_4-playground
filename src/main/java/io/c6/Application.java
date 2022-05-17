eb470ca6e7eaa51df4e2dd9c877187a5c94a8ac6
Chandrasekhar Thotakura <chandu-io>
2022-05-17 11:21:43 +0000

    Initial commit

---
+++ src/main/java/io/c6/Application.java
@@ -0,0 +1,10 @@
+package io.c6;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
